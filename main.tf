72d06dfca6c51ffcced9dff9ac156982eae0edf1
George Zhao <george.zhao@siteminder.com>
2021-03-09 22:28:32 +1100

    feat: support multiple warmers
    Add a rand id to allow multiple warmers

--- main.tf
+++ main.tf
@@ -1,15 +1,19 @@
+resource "random_id" "rand_id" {
+  byte_length = 4
+}
+
 resource "aws_cloudwatch_event_rule" "this" {
   count               = var.enable == true ? 1 : 0
-  name                = "lambda_warmer-${var.function_name}"
+  name                = "lambda_warmer-${var.function_name}-${random_id.rand_id.dec}"
   description         = "Keep warm ${var.function_name} lambda"
   schedule_expression = var.rate
 }
 
 resource "aws_cloudwatch_event_target" "this" {
   count               = "${var.enable == true ? 1 : 0}"
-  rule      = aws_cloudwatch_event_rule.this[0].name
-  target_id = var.function_name
-  arn       = var.function_arn
+  rule                = aws_cloudwatch_event_rule.this[0].name
+  target_id           = var.function_name
+  arn                 = var.function_arn
   input_transformer {
     input_template = var.input
   }
@@ -17,10 +21,10 @@ resource "aws_cloudwatch_event_target" "this" {
 
 resource "aws_lambda_permission" "this" {
   count               = var.enable == true ? 1 : 0
-  statement_id  = "AllowExecutionFromCloudWatch"
-  action        = "lambda:InvokeFunction"
-  function_name = var.function_arn
-  principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.this[0].arn
+  statement_id        = "AllowExecutionFromCloudWatch_${random_id.rand_id.dec}"
+  action              = "lambda:InvokeFunction"
+  function_name       = var.function_arn
+  principal           = "events.amazonaws.com"
+  source_arn          = aws_cloudwatch_event_rule.this[0].arn
 }
 
